3f8db231e9f74c0cf8e9f376b5d62c7316ce9e54
Paul Vorbach <paul@vorb.de>
2013-02-26 12:10:18 +0100

    Change order of definitions

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var persistence = module.exports;
 
 persistence.Schema       = require('./schema').Schema;
 persistence.define       = require('./main').define;
-persistence.setValidator = require('./main').setValidator;
 persistence.getValidator = require('./main').getValidator;
-persistence.setEngine    = require('./main').setEngine;
+persistence.setValidator = require('./main').setValidator;
 persistence.getEngine    = require('./main').getEngine;
+persistence.setEngine    = require('./main').setEngine;
