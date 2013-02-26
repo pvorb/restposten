aecc07e72b131f64704b53b0b24667e5169655bd
Paul Vorbach <paul@vorb.de>
2013-02-26 17:23:09 +0100

    Accessor methods

--- src/schema_instance.js
+++ src/schema_instance.js
@@ -42,6 +42,8 @@ SchemaInstance.prototype.validate = function() {
 
 /**
  * Saves the instance.
+ * 
+ * TODO adjust
  */
 SchemaInstance.prototype.save = function(callback) {
   var errs = this.validate();
@@ -65,6 +67,99 @@ SchemaInstance.prototype.save = function(callback) {
 
 };
 
+/**
+ * TODO adjust
+ */
+SchemaInstance.destroy = function(id, callback) {
+  var key = this.key;
+
+  if (this.schema.properties[key] && this.schema.properties[key].sanitize) {
+    id = this.schema.properties[key].sanitize(id);
+  }
+
+  var newid = this.lowerResource + "/" + id;
+
+  return newid ? this._request('destroy', newid, callback) : callback
+      && callback(new Error('key is undefined'));
+};
+
+/**
+ * TODO adjust
+ */
+SchemaInstance.update = function(id, obj, callback) {
+  var key = this.key;
+
+  if (this.schema.properties[key] && this.schema.properties[key].sanitize) {
+    id = this.schema.properties[key].sanitize(id);
+  }
+
+  if (this._mtime) {
+    obj.mtime = Date.now();
+  }
+
+  var self = this, partialSchema = {
+    properties : {}
+  }, validate;
+
+  // TODO check linking IDs
+  Object.keys(obj).forEach(function(key) {
+    if (self.schema.properties[key]) {
+      partialSchema.properties[key] = self.schema.properties[key];
+    }
+  });
+
+  validate = this.prototype.validate({
+    _properties : obj
+  }, partialSchema);
+
+  if (!validate.valid) {
+    var e = {
+      validate : validate,
+      value : obj,
+      schema : this.schema
+    };
+    this.emit('error', e);
+    if (callback) {
+      callback(e);
+    }
+    return;
+  }
+
+  var newid = this.lowerResource + "/" + id, oldid = id;
+  obj[key] = newid;
+  obj.resource = this._resource;
+
+  return id ? this._request('update', newid, obj, function(err, result) {
+    if (result) {
+      result[key] = oldid;
+      obj[key] = oldid;
+    }
+    callback && callback(err, result);
+  }) : callback && callback(new Error('key is undefined'));
+};
+
+// Define getter / setter for resource property
+SchemaInstance.__defineGetter__('name', function() {
+  return this._name;
+});
+SchemaInstance.__defineSetter__('name', function(name) {
+  return this._name = name;
+});
+
+// Define getter for properties, wraps this resources schema properties
+SchemaInstance.__defineGetter__('schema', function() {
+  return this.schema.properties;
+});
+
+// Define getter / setter for key property. The key property should be defined
+// for all engines
+SchemaInstance.__defineGetter__('key', function() {
+  return this._key || resourceful.key || 'id';
+});
+SchemaInstance.__defineSetter__('key', function(val) {
+  return this._key = val;
+});
+
 // Hooks
 SchemaInstance.after = function(event, callback) {
   this.hook(event, 'after', callback);
@@ -77,4 +172,66 @@ SchemaInstance.before = function(event, callback) {
 SchemaInstance.hook = function(event, timing, callback) {
   this.hooks[timing][event] = this.hooks[timing][event] || [];
   this.hooks[timing][event].push(callback);
+};
+
+SchemaInstance.runBeforeHooks = function(method, obj, callback, finish) {
+  if (method in this.hooks.before) {
+    (function loop(hooks) {
+      var hook = hooks.shift();
+
+      if (hook && hook.length === 2) {
+        hook(obj, function(e, obj) {
+          if (e || obj) {
+            if (callback) {
+              callback(e, obj);
+            }
+          } else {
+            loop(hooks);
+          }
+        });
+      } else if (hook && hook.length === 1) {
+        var res = hook(obj);
+        if (res === true) {
+          loop(hooks);
+        } else {
+          if (callback) {
+            callback(res, obj);
+          }
+        }
+      } else {
+        finish();
+      }
+    })(this.hooks.before[method].slice(0));
+  } else {
+    finish();
+  }
+};
+
+SchemaInstance.runAfterHooks = function(method, e, obj, finish) {
+  if (method in this.hooks.after) {
+    (function loop(hooks) {
+      var hook = hooks.shift();
+
+      if (hook && hook.length === 3) {
+        hook(e, obj, function(e, obj) {
+          if (e) {
+            finish(e, obj);
+          } else {
+            loop(hooks);
+          }
+        });
+      } else if (hook && hook.length === 2) {
+        var res = hook(e, obj);
+        if (res === true) {
+          loop(hooks);
+        } else {
+          finish(res, obj);
+        }
+      } else {
+        finish();
+      }
+    })(this.hooks.after[method].slice(0));
+  } else {
+    finish();
+  }
 };
